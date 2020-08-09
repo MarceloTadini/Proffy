eb5cfa90f3ec4dae3cfb170d0aea496ec458b9d8
Marcelo Tadini <tadini.marcelo53@gmail.com>
2020-08-09 20:26:10 -0300

    Update test.js

--- src/database/test.js
+++ src/database/test.js
@@ -1,8 +1,9 @@
 const Database = require('./db')
+const createProffy = require('./createProffy')
 
-Database.then((db) => {
+Database.then(async(db) => {
     //Inserir dados
-    proffy = {
+    proffyValue = {
         name: "Diego Fernandes",
         avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
         whatsapp: "899989898",
@@ -15,7 +16,7 @@ Database.then((db) => {
         //O proffy id virá pelo banco de dados
     }
 
-    classSchedule = [
+    classScheduleValues = [
         {
             weekday: 1,
             time_from: 720,
@@ -28,7 +29,29 @@ Database.then((db) => {
         }
     ]
 
-    
+    //await createProffy(db, {proffyValue, classValue, classScheduleValues})
 
     //Consultar os dados
+
+    //todos os proffys
+    const selectedProffys = await db.all("SELECT * FROM proffys")
+    console.log(selectedProffys)
+
+    //consultar as classes de um determinado professor e trazer os dados do professor
+    const selectClassesAndProffys = await db.all(`
+        SELECT classes.*, proffys.*
+        FROM proffys 
+        JOIN classes ON (classes.proffy_id = proffys.id)
+        WHERE classes.proffy_id = 1
+    `)
+
+    const selectedClassesSchedules = await db.all(`
+        SELECT class_schedule.*
+        FROM class_schedule
+        WHERE class_schedule.class_id = "1"
+        AND class_schedule.weekday = "0"
+        AND class_schedule.time_from <= "1300"
+        AND class_schedule.time_to > "1300"
+    `)
+
 })
